80902c781fba75ba1f215541e0ea674173d9539e
Felix Wu <mail@felixwu.dev>
2019-06-01 16:48:09 +0200

    Updates

---
+++ precache-manifest.13f7371ce2f974b82297d6eb83ba0b7b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2880ee78c463574a853f1e7c395cbcbf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "49cbea7e8b3e36ed5454",
+    "url": "/static/css/main.ff329414.chunk.css"
+  },
+  {
+    "revision": "4eac0f7b5e3d471aa5fe",
+    "url": "/static/js/2.a5125d37.chunk.js"
+  },
+  {
+    "revision": "49cbea7e8b3e36ed5454",
+    "url": "/static/js/main.d4142988.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  }
+]);
--- src/utils/index.js
+++
@@ -1,4 +0,0 @@
-/* eslint-disable react-hooks/rules-of-hooks */
-const [, updateState] = React => React.useState();
-const forceUpdate = React => React.useCallback(() => updateState({}), []);
-export { forceUpdate };
